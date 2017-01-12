fe866fcba2e9ba80c74ad0450a6cdf1eb1b1f3b3
MaxOvcharov <ovcharovmax@yandex.ru>
2017-01-12 22:39:14 +0300

    bug fixed in hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,13 @@
 def app(environ, start_response):
+    raw_uri = str(environ.get('RAW_URI'))
+    raw_uri = raw_uri[2:]
+    params = raw_uri.split('&')
 	
-	raw_uri = str(environ.get('RAW_URI'))
+    data = ''
+    for param in params:
+        data += param + '\r\n'
 	
-	raw_uri = raw_uri[2:]
-
-	params = raw_uri.split('&')
-	
-	data = ''
-	for param in params:
-		data += param + '\r\n'
-	
-	start_response("200 OK", [
+    start_response("200 OK", [
 	  ("Content-Type", "text/plain"),
 	  ("Content-Length", str(len(data)))
 	])

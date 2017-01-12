4f805337bbe03ca5b78c42014d5eab0c260e5c14
MaxOvcharov <ovcharovmax@yandex.ru>
2017-01-12 22:36:31 +0300

    Added simlink line to init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql start
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py

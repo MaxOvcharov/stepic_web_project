b58ad8e417321d2fece2fe98ee83827cb9823c13
MaxOvcharov <ma12xxx@rambler.ru>
2017-01-15 20:04:57 +0000

    Bug fixed

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -2,17 +2,13 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-from qa.views import login_view, signup, quetion_add, popular, new
 
-from qa import views
-
-urlpatterns = patterns(
-    url(r'^$', views.test),
-    url(r'^login/$', views.test),
-    url(r'^signup/$', views.test),
-    url(r'^question/(\d+)$', views.test),
-    url(r'^ask/.*$', views.test),
-    url(r'^popular/$', views.test),
-    url(r'^new/$', views.test),
-)
+urlpatterns = patterns('',
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/.*$', 'qa.views.test'),
+    url(r'^signup/.*$', 'qa.views.test'),
+    url(r'^question/(\d+)$', 'qa.views.test'),
+    url(r'^ask/.*$', 'qa.views.test'),
+    url(r'^popular/.*$', 'qa.views.test'),
+    url(r'^new/.*$', 'qa.views.test'),
 )

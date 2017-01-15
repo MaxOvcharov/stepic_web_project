c1cce31d7801713419d9d7ac387d8e62d7276e96
MaxOvcharov <ma12xxx@rambler.ru>
2017-01-15 17:18:23 +0000

    Added views settings and some urls to the urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+from qa.views import login_view, signup, quetion_add, popular, new
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +10,11 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', ask.views.home, name='home'),
+    url(r'^login/', login_view, name='login_view'),
+    url(r'^signup/', signup, name='signup'),
+    url(r'^ask/', quetion_add, name='ask'),
+    url(r'^popular/', popular, name='popular'),
+    url(r'^new/', new, name='new'),
+    url(r'^question/(?P<id>\d+/$', quetion_add, name='answer_add'),
 )

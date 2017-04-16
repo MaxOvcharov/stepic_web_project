fddd8f24df614b570166db1270ed7da1507ab28c
MaxOvcharov <ovcharovmax@yandex.ru>
2017-04-16 23:00:58 +0300

    Bug fixed: new urls resolver

---
+++ ask/qa/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('qa.views',
+                       url(r'^question/(?P<qa_id>\d+)/', 'question', name='question'),
+                       url(r'^popular/', 'popular', name='popular'),
+                       url(r'^ask/', 'ask', name='ask'),
+                       url(r'^answer/', 'answer', name='answer'),
+                       url(r'^login/', 'my_login', name='login'),
+                       url(r'^signup/', 'signup', name='signup'),
+                       url(r'^$', 'new_qa', name='new_qa'), )

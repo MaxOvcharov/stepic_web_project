d3c00fb69e6345c93e6aaafc1830a58400e290b6
MaxOvcharov <ovcharovmax@yandex.ru>
2017-04-16 22:57:57 +0300

    Bug fixed: new urls resolver

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,13 +1,26 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', 'qa.views.new_qa'),
-                       url(r'^popular/.*$', 'qa.views.popular_qa'),
-                       url(r'^question/(?P<qa_id>\d+)$', 'qa.views.question'),
-                       url(r'^login/.*$', 'qa.views.test'),
-                       url(r'^signup/.*$', 'qa.views.test'),
-                       url(r'^ask/.*$', 'qa.views.test'),
-                       url(r'^new/.*$', 'qa.views.test'),)
+                       # Examples:
+                       # url(r'^$', 'ask.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^', include('qa.urls')),
+
+                       #    url(r'^question/', include('qa.urls'), name='question'),
+                       #    url(r'^$', 'views.test', name='home'),
+                       #    url(r'^login/', 'views.test', name='login'),
+                       #    url(r'^signup/', 'views.test', name='signup'),
+                       #    url(r'^ask/', 'views.test', name='ask'),
+                       #    url(r'^popular/', 'views.test', name='popular'),
+                       #    url(r'^new/', 'views.test', name='new'),
+
+                       )
+
+
+

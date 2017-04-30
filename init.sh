d2b49d29bb5d732f424ea80b03e9052a85469f52
MaxOvcharov <ovcharovmax@yandex.ru>
2017-04-30 17:56:19 +0000

    Correct init.sh script

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
-#sudo rm /etc/nginx/sites-enabled/default
-#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-#sudo /etc/init.d/nginx restart
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 #sudo /etc/init.d/mysql start
 #sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-#sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
-#sudo /etc/init.d/gunicorn start
+sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
+sudo /etc/init.d/gunicorn start
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE USER 'box'@'localhost'"
 mysql -uroot -e "SET PASSWORD FOR 'box'@'localhost' = PASSWORD('121212')"

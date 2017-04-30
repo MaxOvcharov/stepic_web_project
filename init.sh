7d6623d4f7acc509b1b3009ffaafde33cfa33e73
MaxOvcharov <ovcharovmax@yandex.ru>
2017-04-30 23:36:15 +0300

    Added requirements.txt

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ sudo /etc/init.d/nginx restart
 #sudo /etc/init.d/mysql start
 #sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
-sudo /etc/init.d/gunicorn start
+/etc/init.d/gunicorn start
 sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE USER 'box'@'localhost'"
 mysql -uroot -e "SET PASSWORD FOR 'box'@'localhost' = PASSWORD('121212')"

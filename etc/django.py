d5f571f00caa2ba56ebc56f6cc08ee963170e84c
MaxOvcharov <ovcharovmax@yandex.ru>
2017-04-30 23:24:14 +0300

    Bug fixed: gunicorn conf

--- etc/django.py
+++ etc/django.py
@@ -6,8 +6,6 @@ CONFIG = {
         '--bind=0.0.0.0:8080',
         '--workers=16',
         '--timeout=60',
-        'errorlog="/home/box/logs/error.log"',
-        'loglevel = "degug"',
         'ask.wsgi:application',
     ),
 }
